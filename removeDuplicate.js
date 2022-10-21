88aef4bf02cf46b183f5a2ff000f351b9937ba5d
gamagegit <cgamage92@yahoo.com>
2022-10-20 23:25:50 -0700

    finished Solution

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -7,14 +7,13 @@ function removeDuplicates(nums) {
   let i = 0;
   let temp;
 
-  while (i < nums.length && isNaN(nums[i]) == false) {
+  while (i < nums.length) {
     if (temp === null) {
       temp = nums[i];
       i++;
     } else {
       if (nums[i] === temp) {
         nums.splice(i, 1);
-        nums.push("_");
       } else {
         temp = nums[i];
         i++;
@@ -22,7 +21,7 @@ function removeDuplicates(nums) {
     }
   }
 
-  return nums;
+  return i;
 }
 
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
